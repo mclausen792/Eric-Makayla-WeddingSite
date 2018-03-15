97f05ce05ef2d881f8482b0cf1be64ed5e9bbe6a
Makayla Clausen <mclausen792@gmail.com>
2018-03-15 12:47:03 -0600

    Changed verbiage on RSVP form

--- src/components/RSVP.js
+++ src/components/RSVP.js
@@ -8,14 +8,14 @@ class RSVP extends Component {
       <div className="rsvp">
         <h1 className = "rsvpHead"> Please RSVP by November 1, 2018 </h1>
         <form className = "rsvpForm">
-          <label>Name:</label>
+          <label>Names of Invited Guests</label>
           <input className = "rsvpInput" placeholder="First Name" required/>
           <input className = "rsvpInput" placeholder="Last Name" required/>
           <label>Attending?</label>
           <input className = "rsvpInput" placeholder ="Yes/No" required/>
           <div className = "attending">
-          <input className = "rsvpInput adultNumber" placeholder = "# of Adults"/>
-          <input className = "rsvpInput childNumber" placeholder = "# of Children"/>
+          <input type = "number" className = "rsvpInput adultNumber" placeholder = "# of Adults"/>
+          <input type = "number" className = "rsvpInput childNumber" placeholder = "# of Children"/>
           </div>
           <button>Submit</button>
         </form>
